139e8fa96adca116a6b6d7895b1827553956bb67
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-01-06 01:32:19 +0100

    Load GD SDK before Defold

--- gamedistribution/lib/web/library_gamedistribution.js
+++ gamedistribution/lib/web/library_gamedistribution.js
@@ -7,34 +7,19 @@ var GameDistributionLibrary = {
     },
 
     GameDistribution_PlatformInit: function(gameId, debug) {
-        window["GD_OPTIONS"] = {
-            "gameId": UTF8ToString(gameId),
-            "advertisementSettings": {
-                "debug": (debug == 1), // Enable IMA SDK debugging.
-                "autoplay": false, // Don't use this because of browser video autoplay restrictions.
-                "locale": "en", // Locale used in IMA SDK, this will localize the "Skip ad after x seconds" phrases.
-            },
-        };
-        window["GD_OPTIONS"]["onEvent"] = function(event) {
-            var listener = Context.listener;
-            if (!listener) {
-                console.log("No listener set");
-                return;
+        window["GD_DEFOLD"] = {
+            "onEvent": function(event) {
+                var listener = Context.listener;
+                if (!listener) {
+                    console.log("No listener set");
+                    return;
+                }
+                {{{ makeDynCall("vii", "listener") }}} (
+                    allocate(intArrayFromString(event.name), "i8", ALLOC_STACK),
+                    allocate(intArrayFromString(event.message), "i8", ALLOC_STACK)
+                );
             }
-            {{{ makeDynCall("vii", "listener") }}} (
-                allocate(intArrayFromString(event.name), "i8", ALLOC_STACK),
-                allocate(intArrayFromString(event.message), "i8", ALLOC_STACK)
-            );
         };
-
-        // https://gamedistribution.com/sdk/html5
-        var id = 'gamedistribution-jssdk';
-        var js, fjs = document.getElementsByTagName('script')[0];
-        if (document.getElementById(id)) return;
-        js = document.createElement('script');
-        js.id = id;
-        js.src = 'https://html5.api.gamedistribution.com/main.min.js';
-        fjs.parentNode.insertBefore(js, fjs);
     },
 
     GameDistribution_PlatformSetEventListener: function(listener) {
